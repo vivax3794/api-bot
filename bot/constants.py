4f4eb1c22963416311eb72bd38f6c2a78f7f856f
vivax3794 <51753506+vivax3794@users.noreply.github.com>
2019-08-17 02:31:24 +0200

    changed constants

--- bot/constants.py
+++ bot/constants.py
@@ -1,6 +1,14 @@
 from os import environ
+from logging import getLogger
 
-prefix = environ.get("prefix")
+log = getLogger(__name__)
+
+prefix = environ.get("prefix", ".")
 bot_token = environ.get("bot_token")
+if bot_token is None:
+    log.critical("bot token missing")
+
 
 tracker_token = environ.get("tracker_token")
+if tracker_token is None:
+    log.warning("tracker token missing")
